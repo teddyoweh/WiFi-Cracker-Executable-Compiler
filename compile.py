fe22516a0b810d4fb4ac4993c3fa4adb1c7c690b
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-01-12 00:11:47 -0600

    fix compile bug

--- compile.py
+++ compile.py
@@ -5,15 +5,15 @@ def ensure_pyinstaller_installed():
     try:
         import pyinstaller
     except ImportError:
-        subprocess.check_call(['pip', 'install', 'pyinstaller'])
+        subprocess.check_call(['python3 ', '-m','pip', 'install', 'pyinstaller'])
 
 
 def compile(file_path):
     os_name = os.name
     if os_name == 'nt': # Windows
-        os.system(f'pyinstaller --onefile {file_path}')
+        os.system(f'python3 -m pyinstaller --onefile {file_path}')
     else:
-        os.system(f'pyinstaller --onefile -n {file_path}')
+        os.system(f'python3 -m pyinstaller --onefile -n {file_path}')
 
 ensure_pyinstaller_installed()
 compile(input('Enter the path to file to compile: '))
